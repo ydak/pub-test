ca7f2bbb2c28581d081b0a9d3755e825f7a9e419
ydak <fuzzy.boat0471@fastmail.com>
2023-04-02 20:49:16 +0900

    update

--- gce_minecraft_container.sh
+++ gce_minecraft_container.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# sudo journalctl -u google-startup-scripts.service
 set -eu
 
 echo "Minecraft Server Create Start!"
@@ -38,7 +39,8 @@ echo "Checking latest COS image ..."
 image=$(gcloud compute images list --format="json" | jq -r '.[] | select(.family | test("cos-stable")) | .selfLink' | sed -E 's/.*(projects.*)/\1/')
 
 echo "Creating GCE for minecraft ..."
-gcloud compute instances create minecraft \
+
+external_ip=$(gcloud compute instances create minecraft \
   --format="json" \
   --project="$project_id" \
   --zone=us-west1-b \
@@ -52,15 +54,14 @@ gcloud compute instances create minecraft \
   --create-disk=auto-delete=yes,boot=yes,device-name=minecraft,image=$image,mode=rw,size=10,type="projects/$project_id/zones/us-west1-b/diskTypes/pd-standard" --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring \
   --reservation-affinity=any \
   --metadata=startup-script="#!/bin/bash
-dd if=/dev/zero of=/swap bs=1M count=4096 && \
-mkswap /swap && \
-swapon /swap && \
-chmod -R 600 /swap && \
-echo '/swapfile none swap sw 0 0' >> /etc/fstab && \
 mkdir /var/minecraft && \
 cd /var/minecraft/ && \
 docker volume create mc-volume && \
 docker run -d -it --name mc-server -e EULA=TRUE -p 19132:19132/udp -v mc-volume:/data itzg/minecraft-bedrock-server
-"
+" | jq -r '.[].networkInterfaces.accessConfigs.natIP')
 
 echo "All Done!! Wait for 5 minutes and access the minecraft!"
+
+echo "Your minecraft ip is [$external_ip]"
+
+
